193299fe970e8ab51ff2407156058185387405e5
Matteo Merz <matteo.merz@onyon.com>
2024-03-03 20:42:59 +0100

    chore: enforce config typing

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,31 @@
 import { constants, accessSync, readFileSync } from "fs";
+import { z } from "zod";
 
-const loadConfig = () => {
+const Config = z.object({
+  root: z.string(),
+});
+type Config = z.infer<typeof Config>;
+
+const loadConfig = (): null | Config => {
   const configFilePath = "./route-gen.json";
 
   try {
     accessSync(configFilePath, constants.F_OK);
     const rawConfigData = readFileSync(configFilePath);
 
-    return JSON.parse(rawConfigData.toString());
+    const config = JSON.parse(rawConfigData.toString());
+    return Config.parse(config);
   } catch {
     console.log("Local configuration not found, using default values.");
     return null;
   }
 };
 
-const defaultConfig = {
+const defaultConfig: Config = {
   root: "./src/app",
 };
 
-export const config: Record<string, string> = {
+export const config: Config = {
   ...defaultConfig,
   ...loadConfig(),
 };
